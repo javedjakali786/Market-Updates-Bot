fe6c448d9eddbe7186d83c881f6aa92d78089050
Connor McDonald <connormcd98@gmail.com>
2022-01-13 17:35:34 +0200

    v.1.0.0

--- script.py
+++ script.py
@@ -1,85 +1,14 @@
-import numpy as np
-import pandas as pd
-import requests
-import yfinance as yf
-import datetime as dt
-from datetime import datetime
-from configs import *
-import math
+from functions import *
+
+def main():
+    daily_message = build_text(tickers)
+    for i in send_list: 
+        telegram_bot_sendtext(daily_message, i)
+
+if __name__ == '__main__':
+    main()
 
 
 
-
-def telegram_bot_sendtext(bot_message, chat_id):
-    send_text = 'https://api.telegram.org/bot' + telegram_api_key + '/sendMessage?chat_id=' + chat_id + '&parse_mode=MarkdownV2&text=' + bot_message
-    response = requests.get(send_text)
-    return response.json()
-    
-
-
-lookback = dt.date.today() - dt.timedelta(days=5)
-df = yf.download(tickers, lookback, interval='1d')['Adj Close']
-change = round(100*df.pct_change(),2)
-
-
-btc_price = str(round(df.iloc[-1, 0],2)).replace('.', '\\.').replace('-', '\\-')
-usd_price = str(round(df.iloc[-1, 2],2)).replace('.', '\\.').replace('-', '\\-')
-snp_price = str(round(df.iloc[-1, 3],2)).replace('.', '\\.').replace('-', '\\-')
-eth_price = str(round(df.iloc[-1, 1],2)).replace('.', '\\.').replace('-', '\\-')
-
-btc_change = str(round(change.iloc[-1, 0],2)).replace('.', '\\.').replace('-', '\\-')
-usd_change = str(round(change.iloc[-1, 2],2)).replace('.', '\\.').replace('-', '\\-')
-snp_change = str(round(change.iloc[-1, 3],2)).replace('.', '\\.').replace('-', '\\-')
-eth_change = str(round(change.iloc[-1, 1],2)).replace('.', '\\.').replace('-', '\\-')
-
-daily_message = f"*Daily Update {datetime.today().strftime('%A %d %B %Y')}:* \nBitcoin: ${btc_price}"
-
-if change.iloc[-1, 0] > 0:
-    daily_message += f"\nUp from yesterday by: {btc_change}%\n"
-else:
-    daily_message += f"\nDown from yesterday by: {btc_change}%\n"
-
-
-daily_message += f"\nEthereum: ${eth_price}"
-
-if change.iloc[-1, 1] > 0:
-    daily_message += f"\nUp from yesterday by: {eth_change}%\n"
-else:
-    daily_message += f"\nDown from yesterday by: {eth_change}%\n"
-
-
-
-if math.isnan(df.iloc[-1, 2]):
-    daily_message += f"\nZAR/USD: Market Closed\n"
-else:
-    daily_message += f"\nZAR/USD: R{usd_price}"
-    if change.iloc[-1, 2] > 0:
-        daily_message += f"\nUp from yesterday by: {usd_change}%\n"
-    else:
-        daily_message += f"\nDown from yesterday by: {usd_change}%\n"
-    
-
-
-if math.isnan(df.iloc[-1, 3]):
-    daily_message += f"\nSP500: Market Closed\n"
-else:
-    daily_message += f"\nSP500: ${snp_price}"
-    if change.iloc[-1, 3] > 0:
-        daily_message += f"\nUp from yesterday by: {snp_change}%\n"
-    else:
-        daily_message += f"\nDown from yesterday by: {snp_change}%\n"
-
-FGurl = 'https://api.alternative.me/fng/?limit=1&format=json'
-
-x = requests.get(FGurl)
-index = x.json()
-
-daily_message += f"\nFear and Greed Index: *{index['data'][0]['value']} \({index['data'][0]['value_classification']}\)*\n"
-
-
-
-for i in send_list: 
-    t = telegram_bot_sendtext(daily_message, i)
-    print(t)
 
 
