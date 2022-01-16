c9925b5d091908573fd39cc9bdb318ab847b9c2d
Connor McDonald <connormcd98@gmail.com>
2022-01-16 15:33:20 +0200

    V 1.2

--- example_configs.py
+++ example_configs.py
@@ -1,14 +1,15 @@
 telegram_api_key = 'bot_token_here'
 
-channel_id = '@your_channel_name' #remember to include the @ symbol
-
 chat_id = '2046052520' #use this to send to specific chats instead of channels
 
-crypto_tickers = ['BTC-USD', 'ETH-USD']
-
-stock_tickers = ['BND', 'VNQ','VOO', 'VTI', 'VWO', 'ZAR=X']
+channel_id = '@your_channel_name' #remember to include the @ symbol
 
 keys = {
     'x-rapidapi-host': "fear-and-greed-index.p.rapidapi.com",
     'x-rapidapi-key': "api key"
     }
+
+crypto_tickers = ['BTC-USD', 'ETH-USD']
+
+stock_tickers = ['BND', 'VNQ','VOO', 'VTI', 'VWO', 'ZAR=X']
+
