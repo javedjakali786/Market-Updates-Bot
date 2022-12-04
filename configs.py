23167e7eff6675f3650c2565ae474ffaa6873bc8
Jack Max <44975340+javedjakali786@users.noreply.github.com>
2022-12-05 02:20:54 +0530

    Update and rename example_configs.py to configs.py

---
+++ configs.py
@@ -0,0 +1,15 @@
+telegram_api_key = '5838667173:AAGsJYXYBcaxZoiVJG6yfwwGlrTNxye2s0A'
+
+chat_id = '1001875667016' #use this to send to specific chats instead of channels
+
+channel_id = '@Fear_Greed_Index_Bot' #remember to include the @ symbol
+
+keys = {
+    'x-rapidapi-host': "fear-and-greed-index.p.rapidapi.com",
+    'x-rapidapi-key': "af3e485164mshf83763cb83e468ap1f3f51jsn17ad483f3714"
+    }
+
+crypto_tickers = ['BTC-USD', 'ETH-USD']
+
+stock_tickers = ['BND', 'VNQ','VOO', 'VTI', 'VWO', 'ZAR=X']
+
